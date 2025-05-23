fe9da654191b00c4ac19588c005502722274fa1e
Xima <igorrx24072004@gmail.com>
2025-05-23 13:15:02 -0300

    Adicionando getUserById, and GetUsersByName

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -13,6 +13,29 @@ export const getUsers = (_, res) => {
   });
 };
 
+export const getUserById = (req, res) => {
+  const q = "SELECT * FROM usuarios WHERE `id` = ?";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.json(err);
+
+    if (data.length === 0) return res.status(404).json("Usuário não encontrado.");
+
+    return res.status(200).json(data[0]);
+  });
+};
+
+export const getUsersByName = (req, res) => {
+  const q = "SELECT * FROM usuarios WHERE `nome` LIKE ?";
+  const searchTerm = `%${req.params.name}%`;
+
+  db.query(q, [searchTerm], (err, data) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json(data);
+  });
+};
+
 export const addUser = (req, res) => {
   const q =
     "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
